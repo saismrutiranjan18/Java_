e4b7e635afd8779cac483af8471ad3aa5765f929
Sai Smruti Ranjan Das <saismrutiranjan1810@gmail.com>
2024-07-31 11:49:08 +0530

    delete file

--- 12.Strings/Concatenate.java
+++
@@ -1,8 +0,0 @@
-public class Concatenate {
-    public static void main(String[] args) {
-        String firstName = "sai";
-        String lastName = "das";
-        String fullName = firstName + " " +lastName;
-        System.out.println(fullName);
-    }
-}
