2f06e1472abc1bc1785a45f56797f449093e3e7c
Sai Smruti Ranjan Das <saismrutiranjan1810@gmail.com>
2024-08-01 20:35:11 +0530

    loops

---
+++ 5.Loops (FlowControl)/pracQ6.java
@@ -0,0 +1,7 @@
+public class pracQ6 {
+    public static void main(String[] args) {
+        for (int i= 0 , j = 10; i<j; i++ , j--){
+            System.out.println("i = " +i  +", j = " +j);
+        }
+    }
+}
