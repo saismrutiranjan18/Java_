8d5209291229fa4bdc5a661861b1e15ecfea7f5a
Sai Smruti Ranjan Das <saismrutiranjan1810@gmail.com>
2024-08-05 03:10:17 +0530

    commited

---
+++ 7.Functions & Methods/Prac4.java
@@ -0,0 +1,10 @@
+/* READ & CODE EXERCISE
+Search about(Google) & use the following methods of the Math class in Java:
+a. Math.min( )
+b. Math.max( )
+c. Math.sqrt( )
+d. Math.pow( )
+e. Math.avg( )
+f. Math.abs( )
+Free reading resource (https://www.javatpoint.com/java-math)
+Please feel free to look for more resources/websites on your own.*/
