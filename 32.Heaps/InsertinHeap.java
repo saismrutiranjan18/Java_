e5d1612c62c95ab8b3d77a6180f0e1e96cb0a4ce
Sai Smruti Ranjan Das <160756794+saismrutiranjan18@users.noreply.github.com>
2025-03-12 20:16:04 +0530

    insertinheap

--- 32.Heaps/InsertinHeap.java
+++ 32.Heaps/InsertinHeap.java
@@ -1,30 +1,87 @@
 import java.util.*;
 
-public class InsertinHeap{
+public class InsertinHeap {
 
-    static class Heap{
+    static class Heap {
         ArrayList<Integer> arr = new ArrayList<>();
 
-        public void add(int data){ //O(logn)
-            //add data to the end of the array
+        public void add(int data) { // O(logn)
+            // add data to the end of the array
             arr.add(data);
-            
-            int x = arr.size() - 1; //x is child index
-            int par = (x - 1) / 2; //par is parent index
 
-            while(arr.get(x) < arr.get(par)){ //O(logn)
-                //swap the elements
+            int x = arr.size() - 1; // x is child index
+            int par = (x - 1) / 2; // par is parent index
+
+            while (arr.get(x) < arr.get(par)) { // O(logn)
+                // swap the elements
                 int temp = arr.get(x);
                 arr.set(x, arr.get(par));
                 arr.set(par, temp);
+
+                x = par;
+                par = (x - 1) / 2;
             }
         }
-        public int peek(){ //O(1)
+
+        public int peek() { // O(1)
             return arr.get(0);
+        }
+
+        private void heapify(int i) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            int minIdx = i;
+
+            if (left < arr.size() && arr.get(minIdx) > arr.get(left)) {
+                minIdx = left;
+            }
+
+            if (right < arr.size() && arr.get(minIdx) > arr.get(right)) {
+                minIdx = right;
+            }
+
+            if (minIdx != i) {
+                // swap
+                int temp = arr.get(i);
+                arr.set(i, arr.get(minIdx));
+                arr.set(minIdx, temp);
+
+                heapify(minIdx);
+            }
+
+        }
+
+        public int remove() {
+            int data = arr.get(0);
+
+            // step1 - swap first & last
+            int temp = arr.get(0);
+            arr.set(0, arr.get(arr.size() - 1));
+            arr.set(arr.size() - 1, temp);
+
+            // step2 - delete last
+            arr.remove(arr.size() - 1);
+
+            // step 3 - heapify
+            heapify(0);
+            return data;
+        }
+
+        public boolean isEmpty() {
+            return arr.size() == 0;
         }
     }
 
     public static void main(String[] args) {
-        
+        Heap h = new Heap();
+        h.add(3);
+        h.add(4);
+        h.add(1);
+        h.add(5);
+
+        while (!h.isEmpty()) { // heap sort - O(nlogn)
+            System.out.println(h.peek());
+            h.remove();
+        }
     }
 }
